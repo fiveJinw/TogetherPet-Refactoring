a36c863a3e545057b304a991ce5f80aac55b9f96
fiveJinw <fiveJinw1124@gmail.com>
2025-03-31 21:21:51 +0900

    refactor : TestConventionPlugin 추가

---
+++ build-logic/convention/src/main/java/TestConventionPlugin.kt
@@ -0,0 +1,18 @@
+import com.multi.module.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class TestConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target){
+            dependencies{
+                "testImplementation"(libs.findLibrary("junit").get())
+                "testImplementation"(libs.findLibrary("androidx.arch.core").get())
+                "testImplementation"(libs.findLibrary("kotlinx.coroutines.test").get())
+                "implementation"(libs.findLibrary("io.mockk").get())
+
+            }
+        }
+    }
+}
